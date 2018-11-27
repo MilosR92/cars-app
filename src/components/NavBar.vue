1cad6a709f1212263163fb6afb32770b7cdf48d3
Milos Radivojevic <radivojevicm92@gmail.com>
2018-11-27 20:42:34 +0100

    zadatak1

---
+++ src/components/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <router-link class="navbar-brand" to="/cars">Cars</router-link>
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
---
+++ src/router.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router';
+import AppCars from './components/AppCars.vue'
+
+Vue.config.productionTip = false
+
+Vue.use(VueRouter);
+const routes = [
+    {path:'/', redirect: "/cars"},
+    {path: '/cars', component: AppCars}
+];
+
+const router = new VueRouter({
+    routes
+});
+
+export default router;
+
